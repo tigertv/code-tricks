0d5e1d1ede0ad12168739a409ce2ab1a9d9efa0a
tigertv <maxvetrov555@yandex.ru>
2019-07-15 16:06:19 +0200

    Update arrays, remove count1000

--- c++/arrays.cpp
+++ c++/arrays.cpp
@@ -3,54 +3,66 @@
 
 using namespace std;
 
+void print_line() {
+	cout << "----------------------------------" << endl;
+}
+
 int main()
 {
-	int a[] = {0,1,2,3,4};
+	int a[] = {0, 1, 2, 3, 4};
 	int b = 3;
 	int c = 3;
-	int d[] = {0,1,2,3,4};
-	int e[] = {3,1,2,3,4};
+	int d[] = {5, 6, 7, 8, 9};
+	int e[] = {10, 11, 12, 13, 14};
 
-	b[a] = 1;
-	cout << b[a] << endl;
+	cout << "a[] = {";
+	for (int i = 0; i < 5; i++) {
+		cout << a[i] << ",";
+	}
+	cout << "}" << endl;
 
-	(c+b)[a] = 2;
-	cout << (c+b)[a] << endl;
+	cout << "b = " << b << endl;
+	cout << "c = " << c << endl;
 
-	d[3][a] = 3;
-	cout << d[3][a] << endl;
-	
-	d[c][a] = 4;
-	cout << d[c][a] << endl;
+	cout << "d[] = {";
+	for (int i = 0; i < 5; i++) {
+		cout << d[i] << ",";
+	}
+	cout << "}" << endl;
 
-	c[d][a] = 5;
-	cout << c[d][a] << endl;
+	cout << "e[] = {";
+	for (int i = 0; i < 5; i++) {
+		cout << e[i] << ",";
+	}
+	cout << "}" << endl;
 
-	d[e[0]][a] = 6;
-	cout << d[e[0]][a] << endl;
+	print_line();
 
-	b = 4+0[e][d];
-	cout << b << endl;
+	cout << "b[a] = " << b[a] << endl;
+	cout << "(c+b)[a] = " << (c+b)[a] << endl;
+	cout << "d[3][a] = " << d[3][a] << endl;
+	cout << "d[c][a] = " << d[c][a] << endl;
+	cout << "c[d][a] = " << c[d][a] << endl;
+	cout << "d[e[0]][a] = " << d[e[0]][a] << endl;
+	cout << "4+0[e][d] = " << 4+0[e][d] << endl;
 
 	if (b[a] == a[b]) {
-		cout << 8 << endl;
+		cout << "b[a] == a[b]" << endl;
 	} else {
-		cout << 0 << endl;
+		cout << "b[a] != a[b]" << endl;
 	}
 
-	b = 3*0[e][d];
-	cout << b << endl;
+	cout << "3*0[e][d] = " << 3*0[e][d] << endl;
+	cout << "3*0[e][d] = " << 3*0[e][d] << endl;
+	cout << "d[-3] = " << d[-3] << endl;
 
-	d[-7] = 10;
-	cout << d[-7] << endl;
+	print_line();
 
-	cout << "----------------------------------" << endl;
-
-	for (int i=-7; i<7; i++) {
-		cout << d[i] << endl;
+	for (int i=-10; i<10; i++) {
+		cout << "d[" << i << "] = " << d[i] << endl;
 	}
 
-	cout << "----------------------------------" << endl;
+	print_line();
 
 	return 0;
 }
--- c++/count1000.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(int j) {
-	std::cout << j << std::endl;
-	(main+(int)(exit-(int)(main))*(j/1000))(j+1);
-	return 0;
-}
